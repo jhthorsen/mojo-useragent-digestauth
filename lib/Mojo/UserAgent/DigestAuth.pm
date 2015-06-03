16f7b4333c6efeaef3dff8eea8dfef855b249ee9
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-06-03 10:45:36 +0200

    Released version 0.03
     - Allow setting D-Client-Nonce as header to override random client nonce #1

--- lib/Mojo/UserAgent/DigestAuth.pm
+++ lib/Mojo/UserAgent/DigestAuth.pm
@@ -6,7 +6,7 @@ Mojo::UserAgent::DigestAuth - Allow Mojo::UserAgent to execute digest auth reque
 
 =head1 VERSION
 
-0.02
+0.03
 
 =head1 DESCRIPTION
 
@@ -55,7 +55,7 @@ use Mojo::UserAgent;
 use Mojo::Util 'md5_sum';
 use constant DEBUG => $ENV{MOJO_USERAGENT_DIGEST_AUTH_DEBUG} || 0;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 our @EXPORT  = qw( $_request_with_digest_auth );
 my $NC = 0;
 
